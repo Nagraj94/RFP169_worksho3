92197ce4c76f0205dc8c970883b34bc318ab1007
Nagraj94 <ngowda335@gmail.com>
2022-07-29 11:31:11 +0530

    Deck of cards for four player

---
+++ src/com/bridgelabz/DeckOfCardsMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class DeckOfCardsMain {
+
+    public static void main(String[] args) {
+        DeckOfCards cards = new DeckOfCards();
+        cards.initializeCards();
+        cards.distributeCards();
+        cards.displayCards();
+    }
+
+}
